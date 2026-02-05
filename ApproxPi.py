fddaf4c9aa8ff5a5d5ba9f50d5b88d97007ad750
jackschulz123 <jackschulz2025@gmail.com>
2026-02-05 23:54:14 +0000

    Finished ApproxPi

--- ApproxPi.py
+++ ApproxPi.py
@@ -12,15 +12,36 @@ def main():
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  piDecimals = input("how many decimals of pi would you like to be calculated?(1-10): ")
+  piDecimals = int(piDecimals)
+  isNumberValid = 1
+  while isNumberValid == 1:
+    if 1 <= piDecimals <= 10 :
+      print("Sounds good!")
+      isNumberValid = 2
+    else:
+      print("Invalid number, Try again.")
+      piDecimals = input("How many decimals of pi would you like to be calculated?(1-10): ")
+      piDecimals = int(piDecimals)
+    
 
   start = time.time()
   #calculate pi using the approximation technique
   #Loop until the level of percision is reached
+  approxPi = 4/1
+  sign = -1
+  denom = 3
+  while round(approxPi, piDecimals) != round(realPi,piDecimals):
+    #print(approxPi)
+    approxPi = approxPi + (sign * 4 /denom)
+    sign = sign * -1
+    denom = denom +2
 
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
+  elapsedTime = round(elapsedTime, 5)
+  print("It took", elapsedTime, "seconds")
 
 if __name__ == '__main__':
   main()
